f6ddd9e131597d159a1df024b7d7e51d9b2a0e36
Alexis Mamani <alexis.mamani@oktana.io>
2020-10-03 17:17:08 -0300

    var mongo enviroment

--- server/config/config.js
+++ server/config/config.js
@@ -13,11 +13,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=========================
 let urlDB;
 
-// if( process.env.NODE_ENV === 'dev'){
-//   urlDB = 'mongodb://localhost:27017/cafe';
-// }else{
-  urlDB = 'mongodb+srv://pikabytes:OqPmUTxRtyVyAaF5@pikabot.vs01k.mongodb.net/cafe'
-// }
+if( process.env.NODE_ENV === 'dev'){
+  urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+  urlDB = process.env.MONGO_URI;
+}
 
 
 process.env.URLDB = urlDB;
